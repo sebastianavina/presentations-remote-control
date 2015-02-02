2c36d1f3d59ac72501f388400d5150468fd5c8ab
Sebastian Avina <sebastianavina@gmail.com>
2015-02-02 14:24:50 -0600

    Funcionalidad basica

--- presentacion.py
+++ presentacion.py
@@ -1,7 +1,11 @@
-from bottle import route, run, template, static_file
+from bottle import route, get, post, run, request, template, static_file, redirect
+
+import os
+import commands
 
 import funciones_xdo
 from lecture import lecture
+
 
 # Static dir for serving styles and stuff.
 @route('/static/:path#.+#', name='static')
@@ -9,19 +13,45 @@ def static(path):
     return static_file(path, root='static')
 
 # Main page
-@route('/')
-def index()
-:
-    return template("views/principal.htm", name="hola")
+@get('/')
+def index():
+    xdo = ""
+    if "xdo" in request.query:
+        xdo = request.query["xdo"]
+        funciones_xdo.go_presentation_mode(xdo)
+    ventanas = commands.getstatusoutput("xdotool search \"evince\"",)
+    import re
+    grupos = re.findall(r"([0-9]{8})", str(ventanas))
+    return template("views/principal.htm", name="hola", ventanas = grupos ,xdo = xdo)
+
+@post('/')
+def index():
+    archivo = request.forms.get("presentacion")
+    xdo = request.forms.get("xdo")
+    instruccion = "convert -density 300 %s -resize 200 static/thumbs/%s " % \
+                  (str(archivo), "filminas.png")
+    os.popen(instruccion)
+    redirect("/run/y?file=%s&xdo=%s" % (archivo,xdo))
 
 @route('/prepare/<name>')
 def prepare(name):
     funciones_xdo.go_presentation_mode("27262979")
     return template('<b>Prepare {{name}}</b>!', name=name)
 
-@route('/run/<pagina>')
+@get('/run/<pagina>')
 def run_presentation(pagina):
-    funciones_xdo.change_evince_page(pagina,"27262979")
-    return template('<b>Run {{name}}</b>!', name=name)
+    if "goto" in request.query:
+        xdo = request.query["xdo"]
+        pagina =request.query["goto"]
+        funciones_xdo.change_evince_page(pagina,xdo)
+
+    files = []
+    #funciones_xdo.change_evince_page(pagina,"27262979")
+    filenames = next(os.walk("static/thumbs/"))[2]
+
+    return template("views/mostrar.htm", name=pagina, \
+                    archivos = filenames, \
+                    xdo = request.query["xdo"], \
+                    ventanas = "")
 
 run(host='0.0.0.0', port=8080)
