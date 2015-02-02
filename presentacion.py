2ff708e6478c96a1dc45bca50d162458f73d6dba
Sebastian Avina <sebastianavina@gmail.com>
2015-02-02 11:59:08 -0600

    primera prueba en caliente

--- presentacion.py
+++ presentacion.py
@@ -1,11 +1,23 @@
 from bottle import route, run, template, static_file
 
+import funciones_xdo
+
 @route('/static/:path#.+#', name='static')
 def static(path):
     return static_file(path, root='static')
 
-@route('/hello/<name>')
-def index(name):
-    return template('<b>Hello {{name}}</b>!', name=name)
+@route('/')
+def index():
+    return template('<b>Hello {{name}}</b>!', name="hola")
+
+@route('/prepare/<name>')
+def prepare(name):
+    funciones_xdo.go_presentation_mode("27262979")
+    return template('<b>Prepare {{name}}</b>!', name=name)
+
+@route('/run/<pagina>')
+def run_presentation(pagina):
+    funciones_xdo.change_evince_page(pagina,"27262979")
+    return template('<b>Run {{name}}</b>!', name=name)
 
 run(host='0.0.0.0', port=8080)
