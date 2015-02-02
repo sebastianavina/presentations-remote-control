465eace2591d9ebf99ba1c1f08b94c129461eb98
Sebastian Avina <sebastianavina@gmail.com>
2015-02-02 14:25:10 -0600

    clase para simplificar operaciones

---
+++ lecture.py
@@ -0,0 +1,10 @@
+class lecture:
+    def __init__():
+        self._screens = []
+        self._screen_types = ("evince", "libreoffice", "google-chrome")
+
+    def add_screen(xdoscreen, name, screen_type="evince"):
+        self._screens.append([xdoscreen,name, screen_type])
+
+    def get_screens():
+        return self._screens
